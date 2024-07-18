97a8b7afab5f4420e1efe38e3c8bd7be0c5d61f6
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2024-07-18 19:57:52 -0400

    updates

--- src/firebase-db.js
+++ src/firebase-db.js
@@ -3,20 +3,14 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBXqnunEwKrFxlqRZHRB9VANtG0OihG7TA",
-    authDomain: "whatsapp-clone-ce458.firebaseapp.com",
-    projectId: "whatsapp-clone-ce458",
-    storageBucket: "whatsapp-clone-ce458.appspot.com",
-    messagingSenderId: "221681241925",
-    appId: "1:221681241925:web:46446602bf9acc1e66b82c",
-    measurementId: "G-1KZVSBPCY1"
-  };
-
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const provider = new firebase.auth.GoogleAuthProvider();
+};
 
 
-  export {auth, provider};
-  export default db;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+export {auth, provider};
+export default db;
