b61a8e59923ea15ccdc379fba895678d301f18d6
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2024-07-20 12:52:03 -0400

    app deployed

--- src/Chat.js
+++ src/Chat.js
@@ -4,18 +4,27 @@ import { Avatar, IconButton } from '@mui/material'
 import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@mui/icons-material';
 import { useParams } from 'react-router-dom';
 import db from './firebase';
+import { useStateValue } from './StateProvider';
+import firebase from 'firebase/compat/app';
 
 function Chat() {
     const [input, setInput] = useState("");
     const [seed, setSeed] = useState('');
     const {roomId} = useParams();
     const [roomName, setRoomName] = useState("");
+    const [messages, setMessages] = useState([]);
+    const [{user}, dispatch] = useStateValue();
 
     
     useEffect(() =>{
         if(roomId){
             db.collection('/rooms').doc(roomId).onSnapshot(snapshot => (
-                setRoomName(snapshot.data().name)
+                setRoomName(snapshot.data()?.name)
+            ));
+            db.collection('/rooms').doc(roomId)
+            .collection('/messages')
+            .orderBy('timestamp', 'asc').onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map(doc => doc.data()))
             ));
         }
     }, [roomId]);
@@ -28,6 +37,13 @@ function Chat() {
     const sendMessage = (e) => {
         e.preventDefault();
         console.log('You typed >>>', input);
+
+        db.collection('/rooms').doc(roomId).collection('/messages').add({
+            message: input,
+            name: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        })
+
         setInput('')
     };
 
@@ -37,7 +53,12 @@ function Chat() {
             <Avatar src={`https://api.dicebear.com/9.x/adventurer/svg?seed=${seed}`}/>
             <div className='chat_headerInfo'>
                 <h3>{roomName}</h3>
-                <p>Last seen at...</p>
+                <p>
+                    last seen{" "}
+                    {new Date(
+                        messages[messages.length - 1]?.timestamp?.toDate()).toUTCString()
+                    }
+                </p>
             </div>
             <div className='chat_headerRight'>
                 <IconButton>
@@ -52,18 +73,15 @@ function Chat() {
             </div>
         </div>
         <div className='chat_body'>
-            <p className='chat_message'>
-                Hey guys
-            </p>
-            <p className={`chat_message ${true && 'chat_reciver'}`}>
-                <span className='chat_name'>
-                    Sarfaraz Syed
-                </span>
-                Hey guys
-                <span className='chat_timestamp'>
-                    3:52pm
-                </span>
-            </p>
+            {messages.map( (message) => (
+                <p className={`chat_message ${message.name === user.displayName && 'chat_reciver'}`}>
+                    <span className='chat_name'>{message.name}</span>
+                    {message.message}
+                    <span className='chat_timestamp'>
+                        {new Date(message.timestamp?.toDate()).toUTCString()}
+                    </span>
+                </p>
+            ))}
         </div>
         <div className='chat_footer'>
             <InsertEmoticon />
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -6,6 +6,18 @@ import { Link } from 'react-router-dom';
 
 function SidebarChat({id, name, addNewChat }) {
     const [seed, setSeed] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() =>{
+        if(id){
+            db.collection('/rooms').doc(id)
+            .collection('/messages')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map((doc) => (doc.data())))
+            ))
+        }
+    }, [id]);
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
@@ -28,7 +40,7 @@ function SidebarChat({id, name, addNewChat }) {
                 <Avatar src={`https://api.dicebear.com/9.x/adventurer/svg?seed=${seed}`}/>
                 <div className='sidebarChat_info'>
                     <h2>{name}</h2>
-                    <p>Last message...</p>
+                    <p>{messages[0]?.message}</p>
                 </div>
             </div>
         </Link>
